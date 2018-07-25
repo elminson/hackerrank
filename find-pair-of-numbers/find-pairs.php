3c07bd916fd7d61e336105e6e0700887b1d00824
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-07-25 18:54:45 -0400

    Create find-pairs.php

---
+++ find-pair-of-numbers/find-pairs.php
@@ -0,0 +1,48 @@
+<?php
+class FindPairs
+{
+
+    /**
+     * @param $array
+     */
+    function pair($array, $pair)
+    {
+        $size = count($array);
+        $a[] = $array[0];
+        $found = false;
+        for ($i = 0; $i < $size; $i++) {
+            $number = $pair - $array[$i];
+            if (in_array($number, $array)) {
+                echo "$number, " . $array[$i];
+                $found = true;
+                break; #remove this line if you want to print all pairs found
+            }
+        }
+        if (!$found) {
+            echo "There is no pair that adds up to 10.";
+        }
+
+    }
+
+    /**
+     * @param $array
+     */
+    function pair10($array)
+    {
+        $size = count($array);
+        $a[] = $array[0];
+        $found = false;
+        for ($i = 0; $i < $size; $i++) {
+            $number = 10 - $array[$i];
+            if (in_array($number, $array)) {
+                echo "$number, " . $array[$i];
+                $found = true;
+                break; #remove this line if you want to print all pairs found
+            }
+        }
+        if (!$found) {
+            echo "There is no pair that adds up to 10.";
+        }
+
+    }
+}
